08b7924c1baa3b3bc16fb31010d88f24125c0741
z14165899 <zhao.1988@outlook.com>
2017-09-13 13:54:50 -0700

    Added title tag in handing and CSS for div.

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 
 <head>
     <link rel="stylesheet" href="index.css" type="text/css" />
+    <title>Introduce Myself</title>
 </head>
 
-    <h1>Chen Zhao</h1>
-        <img src="images/chen.jpg" alt="Chen Zhao">
-        <h2>About Me</h2>
-        <p>My name is Chen Zhao from Vancouver of Canada. I previous graduated from University of British Columbia as a Master of Chemical and Biological Enginnering. I now work as a tutor in multiple schools in a nearby city called Richmond and keep learning new knowledge during this teaching process. I am passionate about learning web technologies and computer languages, so I plan to earn a Web Technologies Certificate for myself in order to have opportunities working as a web developer in the future.</p>
-        <h2>Random Quote</h2>
-        <?php
+<h1>Chen Zhao</h1>
+<img src="images/chen.jpg" alt="Chen Zhao">
+<h2>About Me</h2>
+<p>My name is Chen Zhao from Vancouver of Canada. I previous graduated from University of British Columbia as a Master of Chemical and Biological Enginnering. I now work as a tutor in multiple schools in a nearby city called Richmond and keep learning new knowledge during this teaching process. I am passionate about learning web technologies and computer languages, so I plan to earn a Web Technologies Certificate for myself in order to have opportunities working as a web developer in the future.</p>
+<h2>Random Quote</h2>
+<div>
+    <?php
 $strings = array('Welcome Ryan', 'Welcome John', 'Welcome Mark', 'Welcome Mike', 'Welcome Jenna');
 echo $strings[array_rand($strings)];?>
+</div>
