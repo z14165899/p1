1af6ee397f370a132335c7be2e8355ac588cbdcb
z14165899 <30793597+z14165899@users.noreply.github.com>
2017-09-13 15:04:22 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="index.css" type="text/css" />
+</head>
+
+    <h1>Chen Zhao</h1>
+        <img src="images/chen.jpg" alt="Chen Zhao">
+        <h2>About Me</h2>
+        <p>My name is Chen Zhao from Vancouver of Canada. I previous graduated from University of British Columbia as a Master of Chemical and Biological Enginnering. I now work as a tutor in multiple schools in a nearby city called Richmond and keep learning new knowledge during this teaching process. I am passionate about learning web technologies and computer languages, so I plan to earn a Web Technologies Certificate for myself in order to have opportunities working as a web developer in the future.</p>
+        <h2>Random Quote</h2>
+        <?php
+$strings = array('Welcome Ryan', 'Welcome John', 'Welcome Mark', 'Welcome Mike', 'Welcome Jenna');
+echo $strings[array_rand($strings)];?>
